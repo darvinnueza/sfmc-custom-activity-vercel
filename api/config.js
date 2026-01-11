0a2c871a17a288ae2d4773dc33901dea8eadf858
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2026-01-10 23:51:49 -0500

    commit
    commit

--- api/config.js
+++ api/config.js
@@ -1,30 +1,37 @@
 module.exports = (req, res) => {
-    const proto = (req.headers["x-forwarded-proto"] || "https");
+    const proto = req.headers["x-forwarded-proto"] || "https";
     const host = req.headers["x-forwarded-host"] || req.headers.host;
     const baseUrl = `${proto}://${host}`;
 
     res.status(200).json({
         workflowApiVersion: "1.1",
+
         metaData: {
             icon: "https://www.svgrepo.com/show/354243/salesforce.svg",
             category: "message",
             isConfigured: true
         },
+
         type: "REST",
+
         lang: {
-            "en-US": { name: "VoiceBot Demo Activity", description: "Demo Custom Activity (REST) on Vercel" }
+            "en-US": {
+                name: "VoiceBot Demo Activity",
+                description: "Demo Custom Activity (REST) on Vercel"
+            }
         },
+
         arguments: {
             execute: {
                 inArguments: [
-                {
-                    request_id: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.request_id}}",
-                    contact_key: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.contact_key}}",
-                    phone_number: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.phone_number}}",
-                    status: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.status}}",
-                    created_at: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.created_at}}",
-                    updated_at: "{{Contact.Attribute.VOICEBOT_DEMO_CAMPAIGN_1.updated_at}}"
-                }
+                    {
+                        request_id: "{{request_id}}",
+                        contact_key: "{{contact_key}}",
+                        phone_number: "{{phone_number}}",
+                        status: "{{status}}",
+                        created_at: "{{created_at}}",
+                        updated_at: "{{updated_at}}"
+                    }
                 ],
                 outArguments: [],
                 url: `${baseUrl}/api/execute`,
@@ -33,12 +40,31 @@ module.exports = (req, res) => {
                 retryDelay: 0,
                 concurrentRequests: 1
             }
-        },      
+        },
+
+        schema: {
+            arguments: {
+                execute: {
+                    inArguments: [
+                        {
+                        request_id: { dataType: "Text", isNullable: false },
+                        contact_key: { dataType: "Text", isNullable: false },
+                        phone_number: { dataType: "Text", isNullable: true },
+                        status: { dataType: "Text", isNullable: true },
+                        created_at: { dataType: "Date", isNullable: true },
+                        updated_at: { dataType: "Date", isNullable: true }
+                        }
+                    ]
+                }
+            }
+        },
+
         configurationArguments: {
             publish: { url: `${baseUrl}/api/publish` },
             validate: { url: `${baseUrl}/api/validate` },
             stop: { url: `${baseUrl}/api/stop` }
         },
+
         userInterfaces: {
             configurationSupport: `${baseUrl}/index.html`,
             runningSupport: `${baseUrl}/index.html`

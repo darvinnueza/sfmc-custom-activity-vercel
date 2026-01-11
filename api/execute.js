59c88bdf18a9245508cd075253268e48e40f01e2
DARVIN <4372283+darvinnueza@users.noreply.github.com>
2026-01-11 00:36:05 -0500

    commit
    commit

--- api/execute.js
+++ api/execute.js
@@ -1,19 +1,18 @@
 export default async function handler(req, res) {
-    try {
-        // body completo
-        console.log("BODY COMPLETO:", JSON.stringify(req.body, null, 2));
+  try {
+    console.log("BODY COMPLETO:", JSON.stringify(req.body, null, 2));
 
-        const inArgsArray = req.body?.inArguments || [];
-        const merged = Object.assign({}, ...inArgsArray);
+    const inArgsArray = req.body?.inArguments || [];
+    const merged = Object.assign({}, ...inArgsArray);
 
-        console.log("IN_ARGUMENTS (MERGED):", JSON.stringify(merged, null, 2));
+    console.log("IN_ARGUMENTS (MERGED):", JSON.stringify(merged, null, 2));
 
-        return res.status(200).json({
-        status: "ok",
-        received: merged
-        });
-    } catch (err) {
-        console.error("ERROR execute:", err);
-        return res.status(500).json({ status: "error", message: err?.message });
-    }
+    return res.status(200).json({
+      status: "ok",
+      received: merged
+    });
+  } catch (err) {
+    console.error("ERROR execute:", err);
+    return res.status(500).json({ status: "error", message: err?.message });
+  }
 }
